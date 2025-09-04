970c54e5e2ae191b17865467a6e1634cdafb7c83
PRATIK GITTE <96045097+PRATIKGITTE@users.noreply.github.com>
2025-09-04 22:12:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,5 +8,5 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);*/
+);
 
